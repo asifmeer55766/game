76be433a95d5912f33b945346613a4cf20dc6a77
Asif Hussain <158553805+asifmeer55766@users.noreply.github.com>
2025-11-03 20:37:31 +0500

    feat add login and register

---
+++ src/pages/login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function login() {
+  return (
+    <div>
+      <h1>login page only for git and github learning</h1>
+    </div>
+  );
+}
---
+++ src/pages/register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function register() {
+  return (
+    <div>
+      <h2>register page for only git and github learning </h2>
+    </div>
+  )
+}
